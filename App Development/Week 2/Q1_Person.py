190df8489cb331d379c3341d3de86be254b18183
Muhammad Hasif <mhasif175@gmail.com>
2022-10-28 06:40:27 +0800

    lol

---
+++ App Development/Week 2/Q1_Person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, nric):
+        self.__name = name
+        self.__nric = nric
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_nric(self, nric):
+        self.__nric = nric
+
+    def get_nric(self):
+        return self.__nric
