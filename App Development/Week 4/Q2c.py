59f22b7f23b871bbfcf96ec2399cbc1473770883
Muhammad Hasif <mhasif175@gmail.com>
2022-11-11 18:58:33 +0800

    lol

---
+++ App Development/Week 4/Q2c.py
@@ -0,0 +1,36 @@
+try:
+    while True:
+        name = input('Enter name: ')
+        weight = float(input('Enter weight (kg): '))
+        height = float(input('Enter height (m): '))
+
+        bmi = weight/height**2
+        print(bmi)
+
+        command = input('Store your bmi to file? (Y/N): ')
+        if command.upper() == 'Y':
+            bmi_File = open('bmi.txt', 'w')
+            bmi_File.write(name + ',' + str(bmi) + '\n')
+            bmi_File.close()
+
+        command = input('Do you want to continue? (Y/N): ')
+        if command.upper() == 'N':
+            break
+
+    command = input('Do you want to view BMI record in file? (Y/N): ')
+    if command.upper() == 'Y':
+        bmi_File = open('bmi.txt', 'r')
+        contents = bmi_File.read()
+        print(contents)
+        bmi_File.close()
+
+except ValueError:
+    print("Enter a valid input")
+except ZeroDivisionError:
+    print("Do not enter 0 as input")
+except IOError:
+    print("Unable to process file")
+except:
+    print("Error")
+finally:
+    print("End of program")
